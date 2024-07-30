e8d6bf5f0853ef0881271413a3b8faf33013b1d6
Serj Potapov <sky019@inbox.ru>
2024-07-30 18:13:13 +0700

    no message

--- HomeWorks/ContentView.swift
+++ HomeWorks/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, wocrld!")
+            Text("Hello, wocdrld!")
         }
         .padding()
     }
