c7ef54f538c4d26074c6c4d41151c188123657e0
serj019 <71924594+serj019@users.noreply.github.com>
2024-07-30 17:48:19 +0700

    Add files via upload

---
+++ HomeWorks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomeWorks
+//
+//  Created by Serj Potapov on 30.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomeWorks/HomeWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorksApp.swift
+//  HomeWorks
+//
+//  Created by Serj Potapov on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
